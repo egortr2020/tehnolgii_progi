297582904a88f20e79bd59be020652cd038ec65a
egortr2020 <136753951+egortr2020@users.noreply.github.com>
2024-10-25 18:41:59 +0300

    final coomit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import re
 import time
 from dates import *
 
+
+
 bot = telebot.TeleBot('7655400381:AAFIYnMA_7HKJJoi7ight0gwsthjVlj630s')
 
 workbook = openpyxl.load_workbook("24-knt.xlsx")
@@ -45,28 +47,40 @@ def get_schedule_date(selected_day):
     return schedule_date.strftime("%d.%m.%Y")
 
 selected_group = None 
+selected_group_message_id = None
 
 @bot.message_handler(commands=['start'])
 def main(message):
+    current_message_id = message.message_id  
     markup = types.InlineKeyboardMarkup()
     markup.add(types.InlineKeyboardButton('24-кнт', callback_data='24-кнт'))
-    bot.reply_to(message, 'Привет, Выбери свой поток!', reply_markup=markup)
+    sent_message = bot.reply_to(message, 'Привет, Выбери свой поток!', reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda callback: True)
 def handle_callback(callback):
-    global selected_group
-    markup = types.InlineKeyboardMarkup()
-
+    global selected_group, selected_group_message_id
+    message = callback.message
+    chat_id = message.chat.id
+    message_id = message.message_id
     if callback.data == '24-кнт':
-        for group_name in group_columns:
-            markup.add(types.InlineKeyboardButton(group_name.upper(), callback_data=group_name))
-        bot.send_message(callback.message.chat.id, 'Выбери группу:', reply_markup=markup)
+        markup = types.InlineKeyboardMarkup(row_width=2) # Кнопки в два ряда
+        back_button = types.InlineKeyboardButton('⏪ Назад', callback_data='back_to_start')
+        markup.add(*[types.InlineKeyboardButton(group_name.upper(), callback_data=group_name) for group_name in group_columns], back_button)
+        bot.edit_message_text('Выбери группу:', chat_id=chat_id, message_id=message_id, reply_markup=markup)
+    elif callback.data == 'back_to_start': # обработка кнопки назад 
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton('24-кнт', callback_data='24-кнт'))
+        bot.edit_message_text('Привет, Выбери свой поток!', chat_id=chat_id, message_id=message_id, reply_markup=markup)
 
     elif callback.data in group_columns:
         selected_group = callback.data
-        for day_name in day_ranges:
-            markup.add(types.InlineKeyboardButton(day_name.capitalize(), callback_data=day_name))
-        bot.send_message(callback.message.chat.id, 'Выбери день недели:', reply_markup=markup)
+        msg = bot.send_message(chat_id, f"Вы выбрали группу {selected_group.upper()}") 
+        selected_group_message_id = msg.message_id
+
+        markup = types.InlineKeyboardMarkup(row_width=2) 
+        back_button = types.InlineKeyboardButton('⏪ Назад', callback_data='24-кнт')
+        markup.add(*[types.InlineKeyboardButton(day_name.capitalize(), callback_data=day_name) for day_name in day_ranges], back_button)
+        bot.edit_message_text('Выбери день недели:', chat_id=chat_id, message_id=message_id, reply_markup=markup)
     
     elif callback.data in day_ranges and selected_group is not None:
         start_row, end_row = day_ranges[callback.data]
